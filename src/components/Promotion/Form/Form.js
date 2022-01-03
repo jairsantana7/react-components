8637bf165d218073bdd6ad6c9a7086e6a52431f2
unknown <jairsantana7@gmail.com>
2022-01-03 18:10:53 -0300

    implements search

--- src/components/Promotion/Form/Form.js
+++ src/components/Promotion/Form/Form.js
@@ -1,6 +1,28 @@
 import React from "react";
 import "./Form.css";
 
-const PromotionForm = ({ promotion }) => <>FORM</>;
+const PromotionForm = ({ promotion }) => (
+  <>
+    <div className="promotion-search">
+      <header className="promotions-search__header">
+        <h1>Promo Show</h1>
+        <Link to="/create">New Promotion</Link>
+      </header>
+
+      <label htmlFor="title">Título</label>
+
+      <input
+        id="title"
+        className="promotions-search__input"
+        placeholder="Buscar"
+        value={search}
+        onChange={event => {
+          setSearch(event.target.value);
+        }}
+        type="search"
+      />
+    </div>
+  </>
+);
 
 export default PromotionForm;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Root from "./pages/Root";
+import Root from "pages/Root";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
